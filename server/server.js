60e55eb7ed00cd41b915213755b567157f236237
Andrei Scurtu <andrei.scurtu@mindit.io>
2018-10-29 17:10:57 +0200

    Greet new user and alert others

--- server/server.js
+++ server/server.js
@@ -15,14 +15,32 @@ app.use('', express.static(publicPath));
 io.on('connection', socket => {
     console.log('New user connected');
 
+    socket.emit('newMessage', {
+        from: 'admin',
+        text: 'welcome to the chat app',
+        createdAt: Date.now()
+    });
+
+    socket.broadcast.emit('newMessage', {
+        from: 'admin',
+        text: 'new user joined',
+        createdAt: Date.now()
+    });
+
     socket.on('createMessage', message => {
         console.log('createMessage', message);
 
         io.emit('newMessage', {
             from: message.from,
             text: message.text,
-            createdAt: new Date().getTime()
+            createdAt: Date.now()
         });
+
+        // socket.broadcast.emit('newMessage', {
+        //     from: message.from,
+        //     text: message.text,
+        //     createdAt: new Date().getTime()
+        // });
     });       
 
     socket.on('disconnect', () => console.log('User was disconnected'));
